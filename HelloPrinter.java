a65e96b41eed52479f8fe2bae045b256ebc3c77e
sumilongba <60411975+sumilongba@users.noreply.github.com>
2020-01-29 10:53:59 -0500

    Add files via upload

---
+++ HelloPrinter.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloprinter;
+
+/* 
+************************************** 
+*	@author Ben Sumilong * Date: Feb 4, 2020 * CISP 1010 Section: 04N
+*	Time 5:30 – 9:00pm
+*	Classwork Assignment1 HelloPrinter
+**************************************
+*/
+
+public class HelloPrinter 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    
+    {  
+        System.out.println("Hello, Printer!");
+    }
+}
